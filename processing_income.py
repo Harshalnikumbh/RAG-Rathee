6179d4c0a37dee06d9a0bbc5298c0763351c2693
Harshal Nikumbh <harshalnikumbh305@gmail.com>
2025-10-31 17:27:54 +0530

    Reading and laoding embeding joblib file

--- processing_income.py
+++ processing_income.py
@@ -19,9 +19,14 @@ question_embedding = create_embeddings([ask_questions],model)[0]
 # print(np.vstack(df['embedding']).shape)
 
 similarities = cosine_similarity(np.vstack(df['embedding']),[question_embedding]).flatten()
-print(similarities)
+# print(similarities)
 top_results = 3
 max_index = similarities.argsort()[::-1][0:top_results] 
 print(max_index)
 new_df = df.loc[max_index]
-print(new_df[['text','video_title','chunk_id']])
+# print(new_df[['text','video_title','chunk_id']])
+
+for index, item in new_df.iterrows():
+    print(index,item['video_title'],item['chunk_id'],item['text'],
+          item['start_time'],item['end_time'])
+
