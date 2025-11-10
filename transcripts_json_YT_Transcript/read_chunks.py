0f91d6dee7ff99b3f5ed710e2da451f797700ec8
Harshal Nikumbh <harshalnikumbh305@gmail.com>
2025-11-10 11:45:24 +0530

    Add transcripts_json_YT_Transcript module

---
+++ transcripts_json_YT_Transcript/read_chunks.py
@@ -0,0 +1,46 @@
+import json
+import os
+import pandas as pd
+import numpy as np
+import joblib
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from sentence_transformers import SentenceTransformer
+# from sklearn.metrics.pairwise import cosine_similarity
+
+def create_embeddings(text_list, model):
+    """Create embeddings for a list of texts."""
+    embeddings = model.encode(text_list, show_progress_bar=True)
+    return embeddings
+
+# Load model once
+print("Loading model...")
+model = SentenceTransformer('all-MiniLM-L6-v2')
+print("Model loaded!\n")
+
+if __name__ == "__main__":
+    # Get all JSON files
+    jsons = [f for f in os.listdir('transcripts_json') if f.endswith('.json')]
+
+    chunks = []
+
+    for json_file in jsons:
+        with open(f"transcripts_json/{json_file}",encoding='utf-8') as f:
+            content = json.load(f)
+        print("processing: ", json_file)
+        
+        print(f"Creating Embeddings for {json_file}")
+        embeddings = create_embeddings([c['text'] for c in content['chunks']], model)
+        
+        for i, chunk in enumerate(content['chunks']):
+            chunk['video_id'] = content['video_id']
+            chunk['video_title'] = content['video_title']
+            chunk['video_url'] = content['video_url']
+            chunk['duration_minutes'] = content['duration_minutes']
+            chunk['embedding'] = embeddings[i].tolist()
+            chunks.append(chunk)
+    df = pd.DataFrame.from_records(chunks)
+    print(df)
+    # save this df 
+    joblib.dump(df, 'embeddings.joblib')
+
+                                 
