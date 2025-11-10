5ee6d7895341615f391c4dc7416d34eaaba27290
Harshal Nikumbh <harshalnikumbh305@gmail.com>
2025-11-10 16:55:31 +0530

    Restore Flask app run settings to debug mode
    Revert Flask app run configuration to debug mode on port 5000.

--- main.py
+++ main.py
@@ -364,7 +364,7 @@ def stats():
 # MAIN ENTRY POINT
 if __name__ == '__main__':
         # Run the Flask app
-        # app.run(debug=True, host='0.0.0.0', port=5000)
-        import os 
-        port = int(os.environ.get("PORT", 7860))
-        app.run(host='0.0.0.0', port=port, debug=False)
+        app.run(debug=True, host='0.0.0.0', port=5000)
+        # import os 
+        # port = int(os.environ.get("PORT", 7860))
+        # app.run(host='0.0.0.0', port=port, debug=False)
